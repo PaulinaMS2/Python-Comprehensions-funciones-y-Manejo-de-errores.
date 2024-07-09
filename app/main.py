a75cac0ab45629fa24429258483695e00a5d1465
PaulinaMS2 <paulinasaldarriaga34@gmail.com>
2024-07-09 15:16:48 -0500

    Class 20

--- app/main.py
+++ app/main.py
@@ -1,23 +1,30 @@
 import utils #Mi propio modulo
 
-result = utils.get_population()
-print(result)
-
 countries = [
-    {
-        'country': 'colombia',
-        'population': 50000
-    },
-    {
-        'country': 'bolivia',
-        'population': 11000
-    },
-    {
-        'country': 'argentina',
-        'population': 44000
-    }
-]
-country = input("Ingrese el país para obtener su población => ").lower()
+        {
+            'country': 'colombia',
+            'population': 50000
+        },
+        {
+            'country': 'bolivia',
+            'population': 11000
+        },
+        {
+            'country': 'argentina',
+            'population': 44000
+        }
+    ]
 
-result = utils.population_by_country(countries, country)
-print(result)
+def run():
+    result = utils.get_population()
+    print(result)
+
+    country = input("Ingrese el país para obtener su población => ").lower()
+
+    result = utils.population_by_country(countries, country)
+    print(result)
+
+if __name__ == '__main__': #Si se ejecuta este archivo, se ejecuta la función run
+    run()
+
+#Manejar la dualidad de poder ser ejecutados como scripts desde la terminal o ejecutados como modulo en otro archivo
