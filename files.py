99ebf47603a7fc41854b6bb4d18970826e32527c
PaulinaMS2 <paulinasaldarriaga34@gmail.com>
2024-07-09 19:03:48 -0500

    Class 26

--- files.py
+++ files.py
@@ -1,15 +1,21 @@
-file = open('./text.txt')
-# #print(file.read()) #Leerlo completo
+# file = open('./text.txt')
+# # #print(file.read()) #Leerlo completo
 
-# print(file.readline()) #Leer linea por linea de forma manual
-# print(file.readline())
+# # print(file.readline()) #Leer linea por linea de forma manual
+# # print(file.readline())
 
-for line in file:
-    print(line)
+# for line in file:
+#     print(line)
 
-file.close() #Cerrar el archivo
+# file.close() #Cerrar el archivo
 
 #Cerrar automáticamente el archivo
-with open('./text.txt') as file:
+with open('./text.txt', 'w+') as file: #Con el r+ puedes agregar nuevas lineas y el w+ sobreescribe el archivo
     for line in file:
         print(line)
+    #COMO ESCRIBIR EN EL ARCHIVO (WRITE)
+    file.write('\n Nuevas cosas en el archivo') #\n es un salto de linea
+    file.write('\n Nuevas cosas en el archivo otra vez')
+
+
+
