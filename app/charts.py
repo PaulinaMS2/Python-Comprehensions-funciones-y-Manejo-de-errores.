b28d2d72918a1c4dced6b2be6c961ff708ee6c59
PaulinaMS2 <paulinasaldarriaga34@gmail.com>
2024-07-09 22:52:03 -0500

    Class 28

---
+++ app/charts.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+def generate_bar_char(labels, values):
+    fig, ax = plt.subplots()
+    ax.bar(labels, values)
+    plt.show()
+
+def generate_pie_chart(labels, values):
+    fig, ax = plt.subplots()
+    ax.pie(values, labels=labels)
+    ax.axis('equal')
+    plt.show()
+
+if __name__ == '__main__':
+    labels = ['A', 'B', 'C']
+    values = [11, 22, 33]
+    # generate_bar_char(labels, values)
+    generate_pie_chart(labels, values)
